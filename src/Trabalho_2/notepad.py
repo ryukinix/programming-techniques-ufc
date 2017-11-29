f8f16789a8f894015769eace83248210aca83fb1
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-28 23:45:59 -0300

    [Trabalho_2] Adiciona corpo da solução de notepad.py

---
+++ src/Trabalho_2/notepad.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2017 Manoel Vilela
+#
+#    @project: Técnicas de Programação - Trabalho 2
+#     @author: Manoel Vilela
+#      @email: manoel_vilela@engineer.com
+#
+
+
+"""
+Questão 3)
+
+Vamos construir uma aplicação simples de comando de linha de um bloco
+de notas. O problema consiste em: notas curtas são armazenadas no
+bloco de notas, cada nota deve gravar o dia que foi escrita e as tags
+podem ser adicionadas para consultas fáceis. Deve ser possível também,
+modificar as notas e procurar notas.
+
+Autor: Wendley.
+
+"""
+
+
+# layer of compatibility for python2
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
